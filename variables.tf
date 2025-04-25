d1a926f927fa1f2461655e17afc2d56eaf7982f6
2tsiva <147085170+2tsiva@users.noreply.github.com>
2025-04-25 13:21:30 +0100

    add variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "resource_group_name" {
+  default = "sts-rg-1-uks"
+}
+
+variable "linux_virtual_machine_name" {
+  default = "sts-vm-1inux-1-uks"
+}
+
+
+variable "admin_username" {
+  default = "admin_user"
+}
+
+
+variable "admin_password" {
+  default = "set_your_password_here"
+}
